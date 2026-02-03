7cc9502b3971405a7714bf52d242190a3973d1cf
AlexMuia31 <alexmuia31@gmail.com>
2026-02-03 14:24:39 +0300

    adding different textures to a mesh material

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,18 @@
 import { useTexture } from "@react-three/drei";
 
 export const Experience = () => {
-  const texture = useTexture("textures/PavingStones130_1K_Color.jpg");
+  const props = useTexture({
+    map: "textures/PavingStones130_1K_Color.jpg",
+    normalMap: "textures/PavingStones130_1K_NormalGL.jpg",
+    roughnessMap: "textures/PavingStones130_1K_Roughness.jpg",
+    aoMap: "textures/PavingStones130_1K_AmbientOcclusion.jpg",
+  });
 
   return (
     <>
       <mesh>
         <boxGeometry />
-        <meshStandardMaterial map={texture} />
+        <meshStandardMaterial {...props} />
       </mesh>
     </>
   );
